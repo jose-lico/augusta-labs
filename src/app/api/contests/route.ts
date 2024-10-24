249032c7631be4cec4c125241ee9b261a07c0c86
José Lico <lico.jose.pt@gmail.com>
2024-10-24 20:20:22 +0100

    Run Backend Request from nodejs and not edge

--- src/app/api/contests/route.ts
+++ src/app/api/contests/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 
 import mockData from "@/data/contests.json";
 
+export const runtime = "nodejs";
+
 export async function GET() {
   await new Promise((resolve) =>
     setTimeout(resolve, Math.floor(Math.random() * (5000 - 3000 + 1)) + 3000)
